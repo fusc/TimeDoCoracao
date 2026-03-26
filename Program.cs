e8f90cd9f3dff7ceae27efe830822d8cf63e8957
DESKTOP-LN7SD4G\user <user>
2026-03-25 21:29:18 -0300

    Time do coracao

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("FAZE");
+Console.ResetColor();
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("WARZONE");
+Console.ResetColor();
